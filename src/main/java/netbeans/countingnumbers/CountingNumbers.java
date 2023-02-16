082fbfc88a5003b861efbc477eb2158957528738
rbamarc <rbamarc929@gmail.com>
2023-02-15 21:31:52 -0800

    for loops

---
+++ src/main/java/netbeans/countingnumbers/CountingNumbers.java
@@ -0,0 +1,17 @@
+package netbeans.countingnumbers;
+
+/**
+ *
+ * @author halfrikan
+ */
+public class CountingNumbers {
+
+    public static void main(String[] args) {
+        int counter;
+        
+        for ( counter = 1; counter <=21; counter += 2){
+            System.out.println(counter);
+        }
+        System.out.println("....done!");
+    }
+}
