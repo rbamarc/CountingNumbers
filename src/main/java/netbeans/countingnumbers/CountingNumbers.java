62e05c22b84212b217df2850438a954e7a3e8883
rbamarc <rbamarc929@gmail.com>
2023-02-15 21:35:09 -0800

    for loops practice

--- src/main/java/netbeans/countingnumbers/CountingNumbers.java
+++ src/main/java/netbeans/countingnumbers/CountingNumbers.java
@@ -9,7 +9,7 @@ public class CountingNumbers {
     public static void main(String[] args) {
         int counter;
         
-        for ( counter = 1; counter <=21; counter += 2){
+        for ( counter = 2; counter <=21; counter += 2){
             System.out.println(counter);
         }
         System.out.println("....done!");
